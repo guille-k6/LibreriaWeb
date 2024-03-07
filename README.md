1f3eb3994e81928c9eca687f8c0cbdfdb7766a7a
Guillermo <guillermogcasey@gmail.com>
2024-03-06 21:29:34 -0300

    database addition

--- README.md
+++ README.md
@@ -5,17 +5,17 @@ Trabajo práctico integrador de la cátedra de Java (cursado año 2022).
 - Dominguez, Bernardo. Legajo: 43213.
 
 ### Especificaciones
-Sistema web para la administración de una biblioteca. 
+Sistema web para la administración de una biblioteca. El script para crear la base de datos se adjunta como un archivo biblioteca.sql en la carpeta raíz del repositorio.
+
 #### Entidades
 1. **Socio**. Usuario del sistema.
-
-1. **Ejemplar**. Representa la existencia física de un determinado *libro*.
-2. **Libro**. Libro con su nombre *autor* y editorial. Cada libro individualmente tiene una cántidad máxima de días permitidas de préstamo.
-3. **Autor**. Autor de *libros*.
-4. **Política de préstamo**. Representa la política de préstamo actual, es decir, la cántidad máxima de *libros* pendientes de devolución que puede tener un socio.
-5. **Cuota**. Cuotas que un *socio* deberá pagar mensualmente para poder tomar libros *prestados*.
-6. **Préstamo**. Entidad que representa un préstamo, un *socio* podrá realizar un préstamo por uno o varios *libros* y devolverlos todos juntos o separados.
-7. **Línea de préstamo**. Entidad que representa el préstamo de un *libro*, en un *préstamo* hay una o más líneas de préstamo.
+2. **Ejemplar**. Representa la existencia física de un determinado *libro*.
+3. **Libro**. Libro con su nombre *autor* y editorial. Cada libro individualmente tiene una cántidad máxima de días permitidas de préstamo.
+4. **Autor**. Autor de *libros*.
+5. **Política de préstamo**. Representa la política de préstamo actual, es decir, la cántidad máxima de *libros* pendientes de devolución que puede tener un socio.
+6. **Cuota**. Cuotas que un *socio* deberá pagar mensualmente para poder tomar libros *prestados*.
+7. **Préstamo**. Entidad que representa un préstamo, un *socio* podrá realizar un préstamo por uno o varios *libros* y devolverlos todos juntos o separados.
+8. **Línea de préstamo**. Entidad que representa el préstamo de un *libro*, en un *préstamo* hay una o más líneas de préstamo.
 
 #### Caso de uso complejo
 `Préstamo de un libro`. Cuando un usuario se acerca a la biblioteca y elige los libros que tomará como préstamo, el usuario administrador generará un préstamo para ese usuario. En el proceso se validará que el usuario no incumpla la política de préstamo y que tenga la cuota del mes paga.
@@ -33,10 +33,14 @@ Sistema web para la administración de una biblioteca.
 - Administrador: Administrador del sistema que podrá modificar todas las entidades y dar de alta los *préstamos*.  
 
 #### Users para el sistema
-- juanperez : juanperez1 ADMIN
-- johndoe : johndoe1  
-- gcasey : gcasey
-- bdominguez : bdominguez ADMIN
+Formato: Usuario, contraseña (acceso).
 
+>  juanperez, juanperez1 (ADMIN)
+
+> johndoe, johndoe1 (user)
+
+> gcasey, gcasey (user)
+
+> bdominguez, bdominguez (ADMIN)
 
 
--- biblioteca.sql
+++ biblioteca.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `biblioteca` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `biblioteca`;
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
 -- Host: localhost    Database: biblioteca
@@ -28,7 +30,7 @@ CREATE TABLE `autor` (
   `apellido` varchar(45) NOT NULL,
   PRIMARY KEY (`idautor`),
   UNIQUE KEY `idautor_UNIQUE` (`idautor`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +39,7 @@ CREATE TABLE `autor` (
 
 LOCK TABLES `autor` WRITE;
 /*!40000 ALTER TABLE `autor` DISABLE KEYS */;
-INSERT INTO `autor` VALUES (1,'Julio','Verne'),(3,'Antoine','Saint Exuperi'),(4,'William','Shakespeare'),(5,'Charles','Dickens'),(6,'Julio','Verne');
+INSERT INTO `autor` VALUES (16,'Vladimir','Nabokov'),(17,'George','Orwell'),(18,'J.K.','Rowling'),(19,'J.R.R.','Tolkien');
 /*!40000 ALTER TABLE `autor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +60,7 @@ CREATE TABLE `cuotas` (
   PRIMARY KEY (`idcuotas`),
   KEY `FK_cuotasToSocio_idx` (`idSocio`),
   CONSTRAINT `FK_cuotasToSocio` FOREIGN KEY (`idSocio`) REFERENCES `socio` (`idsocio`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +69,7 @@ CREATE TABLE `cuotas` (
 
 LOCK TABLES `cuotas` WRITE;
 /*!40000 ALTER TABLE `cuotas` DISABLE KEYS */;
-INSERT INTO `cuotas` VALUES (7,'2022-12-05','2022-12-02','2023-01-02',4,'Pagada'),(8,'2022-12-05','2022-01-01','2022-02-01',4,'Pagada'),(9,NULL,'2023-01-01','2023-02-01',4,'A_Confirmar'),(10,'2022-12-07','2023-02-01','2023-03-01',4,'Pagada'),(11,NULL,'2023-03-01','2023-04-01',4,'Pendiente'),(12,NULL,'2022-03-01','2023-04-01',5,'A_Confirmar'),(13,NULL,'2022-04-01','2023-05-01',5,'Pendiente'),(14,NULL,'2022-05-01','2023-06-01',5,'Pendiente'),(15,NULL,'2022-12-01','2023-01-01',5,'Pendiente'),(16,NULL,'2023-01-01','2023-02-01',5,'Pendiente'),(17,NULL,'2023-02-01','2023-03-01',5,'Pendiente');
+INSERT INTO `cuotas` VALUES (7,'2023-08-15','2022-12-02','2023-01-02',4,''),(8,'2023-08-15','2022-01-01','2022-02-01',4,''),(9,'2024-01-23','2022-12-02','2023-02-02',4,'Pagada'),(10,'2024-02-23','2022-12-02','2023-02-02',4,'Pagada'),(11,'2024-02-23','2023-03-15','2023-05-15',4,'Pagada'),(12,'2024-02-27','2023-06-10','2023-08-10',4,'Pagada'),(15,NULL,'2024-03-01','2024-04-01',5,'A_Confirmar'),(16,NULL,'2024-04-01','2024-05-01',5,'A_Confirmar'),(21,'2024-03-03','2024-03-05','2024-04-05',1,'Pagada'),(22,'2024-03-06','2024-03-05','2024-04-05',2,'Pagada'),(23,NULL,'2024-03-05','2024-04-05',4,'Pendiente');
 /*!40000 ALTER TABLE `cuotas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,11 +83,12 @@ DROP TABLE IF EXISTS `ejemplar`;
 CREATE TABLE `ejemplar` (
   `idejemplar` int NOT NULL AUTO_INCREMENT,
   `idLibro` int unsigned NOT NULL,
+  `disponible` tinyint DEFAULT '1',
   PRIMARY KEY (`idejemplar`),
   UNIQUE KEY `idejemplar_UNIQUE` (`idejemplar`),
   KEY `FK_EjemplarToLibro_idx` (`idLibro`),
   CONSTRAINT `FK_EjemplarToLibro` FOREIGN KEY (`idLibro`) REFERENCES `libro` (`idlibro`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,7 +97,7 @@ CREATE TABLE `ejemplar` (
 
 LOCK TABLES `ejemplar` WRITE;
 /*!40000 ALTER TABLE `ejemplar` DISABLE KEYS */;
-INSERT INTO `ejemplar` VALUES (1,1),(5,4),(4,6);
+INSERT INTO `ejemplar` VALUES (25,12,0),(26,12,1),(27,12,1),(28,12,1),(29,12,1),(30,13,0),(31,13,0),(32,13,0),(33,13,0),(34,13,0),(35,13,0),(36,14,1),(37,14,1),(38,15,1),(39,14,1),(40,13,1),(41,14,1),(42,13,1);
 /*!40000 ALTER TABLE `ejemplar` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +120,7 @@ CREATE TABLE `libro` (
   UNIQUE KEY `idlibro_UNIQUE` (`idlibro`),
   KEY `FK_LibroToAutor_idx` (`idAutor`),
   CONSTRAINT `FK_LibroToAutor` FOREIGN KEY (`idAutor`) REFERENCES `autor` (`idautor`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +129,7 @@ CREATE TABLE `libro` (
 
 LOCK TABLES `libro` WRITE;
 /*!40000 ALTER TABLE `libro` DISABLE KEYS */;
-INSERT INTO `libro` VALUES (1,'ABC1234','8787878787','Luz','2020-09-12',90,1),(4,'AA1233JA','El psicoanalista','Wallace','2012-07-17',9,5),(6,'9009090','100 anios de soledad','Luz','2020-10-13',0,3);
+INSERT INTO `libro` VALUES (12,'978-0451524935','1984','Signet Classic','1950-07-01',14,17),(13,'978-0545162074','Harry Potter and the Sorcerer Stone','Scholastic','1998-09-01',14,18),(14,'978-0547928227','The Hobbit','Houghton Mifflin Harcourt','2012-09-18',14,19),(15,'958-0451525335','Lolita','Marquez','2001-11-26',30,16);
 /*!40000 ALTER TABLE `libro` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,7 +144,6 @@ CREATE TABLE `lineadeprestamo` (
   `idlineadeprestamo` int NOT NULL AUTO_INCREMENT,
   `fechaDevolucionTeorica` date NOT NULL,
   `fechaDevolucionReal` date DEFAULT NULL,
-  `estadoLinea` varchar(45) NOT NULL,
   `idPrestamo` int unsigned NOT NULL,
   `idEjemplar` int NOT NULL,
   PRIMARY KEY (`idlineadeprestamo`),
@@ -150,7 +152,7 @@ CREATE TABLE `lineadeprestamo` (
   KEY `FK_lineadeprestamoToPrestamo_idx` (`idPrestamo`),
   CONSTRAINT `FK_lineadeprestamoToEjemplar` FOREIGN KEY (`idEjemplar`) REFERENCES `ejemplar` (`idejemplar`) ON DELETE RESTRICT ON UPDATE CASCADE,
   CONSTRAINT `FK_lineadeprestamoToPrestamo` FOREIGN KEY (`idPrestamo`) REFERENCES `prestamo` (`idprestamo`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,35 +161,8 @@ CREATE TABLE `lineadeprestamo` (
 
 LOCK TABLES `lineadeprestamo` WRITE;
 /*!40000 ALTER TABLE `lineadeprestamo` DISABLE KEYS */;
+INSERT INTO `lineadeprestamo` VALUES (22,'2024-03-20','2024-03-06',10,25),(23,'2024-03-20','2024-03-06',10,30),(24,'2024-03-20',NULL,11,32),(25,'2024-03-20',NULL,11,33),(26,'2024-03-20',NULL,11,30),(27,'2024-03-20',NULL,11,31),(28,'2024-03-20',NULL,12,34),(29,'2024-03-20',NULL,12,35),(30,'2024-03-20',NULL,13,25);
 /*!40000 ALTER TABLE `lineadeprestamo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `lineadeventa`
---
-
-DROP TABLE IF EXISTS `lineadeventa`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `lineadeventa` (
-  `idlineadeventa` int NOT NULL AUTO_INCREMENT,
-  `idVenta` int NOT NULL,
-  `idEjemplar` int NOT NULL,
-  PRIMARY KEY (`idlineadeventa`),
-  KEY `FK_lineadeventaToEjemplar_idx` (`idEjemplar`),
-  KEY `FK_lineadeventaToVenta_idx` (`idVenta`),
-  CONSTRAINT `FK_lineadeventaToEjemplar` FOREIGN KEY (`idEjemplar`) REFERENCES `ejemplar` (`idejemplar`) ON DELETE RESTRICT ON UPDATE CASCADE,
-  CONSTRAINT `FK_lineadeventaToVenta` FOREIGN KEY (`idVenta`) REFERENCES `venta` (`idventa`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `lineadeventa`
---
-
-LOCK TABLES `lineadeventa` WRITE;
-/*!40000 ALTER TABLE `lineadeventa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `lineadeventa` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -202,7 +177,7 @@ CREATE TABLE `politicaprestamo` (
   `fechaDesde` date NOT NULL,
   `cantMaxLibrosPend` int NOT NULL,
   PRIMARY KEY (`idPoliticaPrestamo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -211,6 +186,7 @@ CREATE TABLE `politicaprestamo` (
 
 LOCK TABLES `politicaprestamo` WRITE;
 /*!40000 ALTER TABLE `politicaprestamo` DISABLE KEYS */;
+INSERT INTO `politicaprestamo` VALUES (4,'2024-02-06',5),(9,'2023-06-07',9);
 /*!40000 ALTER TABLE `politicaprestamo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -254,7 +230,7 @@ CREATE TABLE `prestamo` (
   UNIQUE KEY `idprestamo_UNIQUE` (`idprestamo`),
   KEY `FK_prestamoToSocio_idx` (`idSocio`),
   CONSTRAINT `FK_prestamoToSocio` FOREIGN KEY (`idSocio`) REFERENCES `socio` (`idsocio`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -263,35 +239,8 @@ CREATE TABLE `prestamo` (
 
 LOCK TABLES `prestamo` WRITE;
 /*!40000 ALTER TABLE `prestamo` DISABLE KEYS */;
+INSERT INTO `prestamo` VALUES (1,'2023-07-19',2),(2,'2023-08-14',2),(3,'2024-02-23',1),(4,'2024-02-24',1),(5,'2024-02-24',1),(6,'2024-02-26',1),(7,'2024-02-27',5),(8,'2024-02-28',4),(9,'2024-02-29',4),(10,'2024-03-06',4),(11,'2024-03-06',2),(12,'2024-03-06',2),(13,'2024-03-06',4);
 /*!40000 ALTER TABLE `prestamo` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sancion`
---
-
-DROP TABLE IF EXISTS `sancion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `sancion` (
-  `idsancion` int unsigned NOT NULL AUTO_INCREMENT,
-  `fechaSancion` date NOT NULL,
-  `diasSancion` int NOT NULL,
-  `idSocio` int NOT NULL,
-  PRIMARY KEY (`idsancion`),
-  UNIQUE KEY `idsancion_UNIQUE` (`idsancion`),
-  KEY `FK_sancionToSocio_idx` (`idSocio`),
-  CONSTRAINT `FK_sancionToSocio` FOREIGN KEY (`idSocio`) REFERENCES `socio` (`idsocio`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sancion`
---
-
-LOCK TABLES `sancion` WRITE;
-/*!40000 ALTER TABLE `sancion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sancion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -308,13 +257,12 @@ CREATE TABLE `socio` (
   `email` varchar(45) NOT NULL,
   `domicilio` varchar(45) DEFAULT NULL,
   `telefono` varchar(45) DEFAULT NULL,
-  `estadoSocio` varchar(45) NOT NULL,
-  `contrasenia` varchar(45) NOT NULL,
+  `contrasenia` varchar(256) NOT NULL,
   `isAdmin` tinyint NOT NULL DEFAULT '0',
   `usuario` varchar(45) NOT NULL,
   PRIMARY KEY (`idsocio`),
   UNIQUE KEY `idsocio_UNIQUE` (`idsocio`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -323,7 +271,7 @@ CREATE TABLE `socio` (
 
 LOCK TABLES `socio` WRITE;
 /*!40000 ALTER TABLE `socio` DISABLE KEYS */;
-INSERT INTO `socio` VALUES (1,'Perez','Juan','juan@perz.com','Av 1234','12345678','Activo','juanperez1',1,'juanperez'),(2,'Doe','John','john@doe.com','Av 66544','3783748374','Activo','johndoe1',1,'johndoe'),(4,'noAdmin','noAdmin','noadmin@gmail.com','asdasd','3423432432','Pendiente','noadmin',0,'juanNoAdmin'),(5,'Casey','Guillermo','guillermocasey@hotmail.com','Pellegrini 233','3323232','Activo','guillermo',0,'guillek6');
+INSERT INTO `socio` VALUES (1,'Perez','Juan','juan@perz.com','Av 1234','12345678','50eec946d18107695ed9965c772d01c8ddd63b2aa53adfa69f2d7cac114b8e1b',1,'juanperez'),(2,'Doe','John','john@doe.com','Av 66544','3783748374','ec08a62bede177c9307f5c827847697ea7eb32bde9d17ba585929e36af58b44d',0,'johndoe'),(4,'Guillermo','Casey','guillermocasey@gmail.com','Av siempreviva 123','3423432432','4f1b6d801016c7731be4d7d8e951e156786ac469ed03e9dbe3e096a12e32e195',0,'gcasey'),(5,'Bernardo','Dominguez','bernidom@gmail.com','AV berni 123','23123438','02a941fd4718195c221d3480bd1a132d0f3ef78923bfba19572ddddea7e460ca',1,'bdominguez');
 /*!40000 ALTER TABLE `socio` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -351,34 +299,6 @@ LOCK TABLES `valorcuotas` WRITE;
 INSERT INTO `valorcuotas` VALUES ('2020-01-01',300),('2022-01-01',400);
 /*!40000 ALTER TABLE `valorcuotas` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `venta`
---
-
-DROP TABLE IF EXISTS `venta`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `venta` (
-  `idventa` int NOT NULL AUTO_INCREMENT,
-  `fechaVenta` date NOT NULL,
-  `precioTotal` int DEFAULT NULL,
-  `idSocio` int NOT NULL,
-  PRIMARY KEY (`idventa`),
-  UNIQUE KEY `idventa_UNIQUE` (`idventa`),
-  KEY `FK_ventaToSocio_idx` (`idSocio`),
-  CONSTRAINT `FK_ventaToSocio` FOREIGN KEY (`idSocio`) REFERENCES `socio` (`idsocio`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `venta`
---
-
-LOCK TABLES `venta` WRITE;
-/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
-/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -389,4 +309,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-07 16:45:19
+-- Dump completed on 2024-03-06 21:28:17
